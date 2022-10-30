86ad0f6e9ae2e640457da8c6f0f8fe16ecf1745b
Lukas Pieper <30661176+lukaspieper@users.noreply.github.com>
2022-10-30 16:52:50 +0100

    Add MIT license

--- lib/src/main/java/de/lukaspieper/crypto/pink/argon2/Argon2Config.kt
+++ lib/src/main/java/de/lukaspieper/crypto/pink/argon2/Argon2Config.kt
@@ -1,3 +1,8 @@
+// Copyright (c) 2021 Lukas Pieper
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 package de.lukaspieper.crypto.pink.argon2
 
 internal interface Argon2Config {
