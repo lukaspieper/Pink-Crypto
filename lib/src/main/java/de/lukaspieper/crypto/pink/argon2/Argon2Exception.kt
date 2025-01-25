72aada6c40b8164e9c929ed1e5d7c73154d01bef
Lukas Pieper <30661176+lukaspieper@users.noreply.github.com>
2025-01-25 14:20:16 +0100

    Remove Pink-Crypto source code

--- lib/src/main/java/de/lukaspieper/crypto/pink/argon2/Argon2Exception.kt
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021 Lukas Pieper
-//
-// This source code is licensed under the MIT license found in the
-// LICENSE file in the root directory of this source tree.
-
-package de.lukaspieper.crypto.pink.argon2
-
-public class Argon2Exception : Exception()
